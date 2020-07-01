14919babe0c5c93a3c696f1dd390129ee2167e8f
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-01 16:57:06 -0300

    Projeto simples para criação de contas de banco. Exercício para aprendizado de POO (Programação Orientada a Objetos) em C#.

---
+++ Modulo5/Models/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Modulo5.Models
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        private double Saldo;
+
+        private static double Taxa = 5;
+
+        public Conta(int numeroConta, string nomeTitular, double saldo)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia; 
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + Taxa;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {NumeroConta}, Titular: {NomeTitular}, Saldo R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Modulo5/Program.cs
@@ -0,0 +1,56 @@
+﻿using Modulo5.Models;
+using System;
+
+namespace Modulo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int NumeroTitular = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("Haverá Depósito Inicial (s/n)? ");
+            char deposita = char.Parse(Console.ReadLine());
+
+            double valorDoDeposito = depositoInicial(deposita);
+
+            Conta conta1 = new Conta(NumeroTitular, nomeTitular, valorDoDeposito);
+
+            Console.WriteLine(conta1.ToString());
+
+            Console.Write("Entre com o valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+
+            conta1.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta1.ToString());
+
+            Console.Write("Entre com o valor para saque: ");
+            double saque = double.Parse(Console.ReadLine());
+                        
+            conta1.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta1.ToString());
+
+            double depositoInicial(char dep)
+            {
+                double valor;
+                if (dep == 's')
+                {
+                    Console.Write("Entre com o valor do depósito Inicial: ");
+                    valor = double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.Write("Sem depósito inicial!");
+                    valor = 0;
+                }
+                return valor;
+            }
+        }
+    }
+}
